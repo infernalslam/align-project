f17f1956b71aa6d60f74b46eff818110eee1e2ab
infernalslam <infernal-slam@hotmail.com>
2017-09-07 12:24:49 +0700

    firsty

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <header-view />
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import headerView from './components/headerView'
+export default {
+  name: 'app',
+  components: {
+    headerView
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+</style>
---
+++ src/components/headerView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="header-view">
+    <el-breadcrumb separator="/">
+      <el-breadcrumb-item>New +</el-breadcrumb-item>
+      <el-breadcrumb-item>List Working</el-breadcrumb-item>
+      <el-breadcrumb-item>Schedule</el-breadcrumb-item>
+    </el-breadcrumb>
+
+  </div>
+</template>
+<script>
+export default {
+  name: 'header-view'
+}
+</script>
+<style scoped>
+#header-view {
+}
+</style>
